0a52cf291d518157eacbb729d03f3962f4f381fa
MarifulIslam2123 <remudhaka@gmail.com>
2023-12-28 14:47:18 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Accor from "./component/Accor"
+import Card from "./component/Card"
+import Mask from "./component/Mask"
+
+function App() {
+  
+  return (
+    <>
+      <Card/>
+      <Accor/>
+      <Mask/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Mask.jsx
@@ -0,0 +1,9 @@
+function Mask() {
+  return (
+    <div>
+        <img className="mask mask-squircle" src="https://daisyui.com/images/stock/photo-1567653418876-5bb0e566e1c2.jpg" />
+    </div>
+  )
+}
+
+export default Mask
